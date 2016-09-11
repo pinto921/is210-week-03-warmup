c6bb1f3ecd9ceca2800e17efee34423ee6a01408
pinto921 <pinto921@hotmail.com>
2016-09-11 05:57:06 +0000

    task 12 fractions

---
+++ task_12.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import decimal
+import fractions
+
+INTVAL = 1
+FLOATVAL = 0.1
+DECVAL = decimal.Decimal ('0.1')
+FRACVAL = fractions.Fractions ('1/10')
+
+
