c28f884915cbdd06d771fa951d6516b548af3497
pinto921 <pinto921@hotmail.com>
2016-09-11 05:24:06 +0000

    docstring task 09

--- task_09.py
+++ task_09.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """A computer that can build the question."""
+"""
+
+
+A computer that can build the question
+
+doc string to show
+
+task 9 done good bye
+
+"""
 
 THE_ANSWER_TO_EVERYTHING = 42
 
