9f9b94d820ad6aa60e17c8ddc974b8bbde4c9f59
pinto921 <pinto921@hotmail.com>
2016-09-11 03:55:05 +0000

    TURTLE excercise

--- task_05.py
+++ task_05.py
@@ -3,5 +3,7 @@
 """Splinter would be proud."""
 
 
-TEENAGE_MUTANT_NINJAS = ('Michaelangelo. Leonardo. Rafael. Donatello. Heroes '
+TEENAGE_MUTANT_NINJAS =  ('Michaelangelo. Leonardo. Rafael. Donatello. Heroes '
                          'in a half shell.')
+DELIMETER = '.'
+TURTLE_POWER = TEENAGE_MUTANT_NINJAS.split(DELIMETER)
